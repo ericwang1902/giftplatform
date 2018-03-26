19a858a996cebdcb35a057d84f94e1bca4e1ee06
ABC_CODER <designer.shaoyan@gmail.com>
2018-03-26 12:12:48 +0800

    1. 添加登录视图逻辑。

--- gift_platform_site/views.py
+++ gift_platform_site/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate,login
 from django.views import View
 
 class LoginView(View):
@@ -7,5 +7,18 @@ class LoginView(View):
         return render(request,"sign/login.html")
 
     def post(self, request):
-        pass
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            # 如果用户不为空，则继续检查该账户类型，只能由商户登录进入
+            if user.type is 'giftcompany':
+                login(request, user)
+            else:
+                return render(request, 'sign/login.html', {
+                    'error_message': "用户名或者密码错误"
+                })
+        else:
+            # 用户名或者密码错误
+            return render(request, 'sign/login.html', {
+                'error_message': "用户名或者密码错误"
+            })
 
