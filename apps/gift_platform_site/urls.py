e1f888794ab16aca1a3b4a64f7f910fda1f7c00e
ericwang1902 <ericwang1902@163.com>
2018-04-08 00:01:24 +0800

    加上系统消息的页面

--- apps/gift_platform_site/urls.py
+++ apps/gift_platform_site/urls.py
@@ -4,7 +4,7 @@ from apps.gift_platform_site.views import IndexView
 from apps.gift_platform_site.views import MyaccountView,ModifyPwdView,logoutView, brands_list, categories_list, category_product_list, root_category_product_list, search_products
 from apps.gift_platform_site.views import search_supplier
 
-from apps.gift_platform_site.views import msgCenterView
+from apps.gift_platform_site.views import msgCenterView,sysinfoView
 
 app_name = 'gift_platform_site'
 
@@ -28,6 +28,7 @@ urlpatterns =  [
 
     path('usercenter/myaccount',MyaccountView.as_view(),name='myaccount'),
     path('usercenter/mpwd',ModifyPwdView.as_view(),name="modifypwd"),
+    path('usercenter/sysinfo',sysinfoView.as_view(),name='sysinfo')
 
 ]
 
