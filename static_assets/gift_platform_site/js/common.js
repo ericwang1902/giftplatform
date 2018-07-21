db1b4933ee34efc2f7d759958daca8c5563e77a9
ABC_CODER <designer.shaoyan@gmail.com>
2018-07-21 09:25:14 +0800

    更新
    1. 添加首页模板。

---
+++ static_assets/gift_platform_site/js/common.js
@@ -0,0 +1,18 @@
+﻿$(function(){
+	
+	$(".nav ul li").hover(function () {
+		if($(this).find(".item").find("p").length>0){
+			$(this).find(".item").show();
+		}
+	}, function () {
+		$(this).find(".item").hide();
+	});
+})
+function setTabGG(name, cursel, n) {
+	for (i = 1; i <= n; i++) {
+		var menu = document.getElementById(name + i);
+		var con = document.getElementById("lmu_" + name + "_" + i);
+		menu.className = i == cursel ? "act" : "";
+		con.style.display = i == cursel ? "block" : "none";
+	}
+}
