68018a3aa79260e60611d505339b6c995c01652c
ABC_CODER <designer.shaoyan@gmail.com>
2018-09-05 20:49:13 +0800

    [BUG FIXED] 修复商品描述图片可能重复的问题。

--- apps/products/api/views.py
+++ apps/products/api/views.py
@@ -24,10 +24,11 @@ class ProductDescriptionMedia(APIView):
         dest_folder = os.path.join(MEDIA_ROOT, 'products/description/')
         if not os.path.exists(dest_folder):
             os.makedirs(dest_folder)
-        with open(os.path.join(dest_folder, '{0}_{1}'.format(uuid.uuid4(), file.name)), 'wb+') as destination:
+        filename = '{0}_{1}'.format(uuid.uuid4(), file.name)
+        with open(os.path.join(dest_folder, filename), 'wb+') as destination:
             for chunk in file.chunks():
                 destination.write(chunk)
-        return Response({'success': True, 'msg': 'ok', 'file_path': ('http://' + request.get_host() + '/media/products/description/{0}'.format(file.name))})
+        return Response({'success': True, 'msg': 'ok', 'file_path': ('http://' + request.get_host() + '/media/products/description/{0}'.format(filename))})
 
 
 class brandsList(generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin):
